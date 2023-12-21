0a59a084a5d77076d24f563317da5c64a92597b1
ablofazl <aboli138400@gmail.com>
2023-12-21 15:36:46 +0330

    initial commit

---
+++ src/app/app.module.ts
@@ -0,0 +1,62 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {MatSidenav, MatSidenavModule} from "@angular/material/sidenav";
+import {MatGridListModule} from "@angular/material/grid-list";
+import {MatMenuModule} from "@angular/material/menu";
+import {MatButtonModule} from "@angular/material/button";
+import {MatCardModule} from "@angular/material/card";
+import {MatIconModule} from "@angular/material/icon";
+import {MatExpansionModule} from "@angular/material/expansion";
+import {MatToolbarModule} from "@angular/material/toolbar";
+import {MatTableModule} from "@angular/material/table";
+import {MatBadgeModule} from "@angular/material/badge";
+import {MatSnackBarModule} from "@angular/material/snack-bar";
+import { HeadComponent } from './components/head/head.component';
+import { HomeComponent } from './pages/home/home.component';
+import { ProductsHeaderComponent } from './pages/home/component/products-header.component';
+import { FiltersComponent } from './pages/home/component/filters/filters.component';
+import {MatListModule} from "@angular/material/list";
+import { ProductBoxComponent } from './pages/home/component/product-box/product-box.component';
+import { CartComponent } from './pages/cart/cart.component';
+import {CartService} from "../services/cart.service";
+import {StoreService} from "../services/store.service";
+import {HttpClient, HttpClientModule} from "@angular/common/http";
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    HeadComponent,
+    HomeComponent,
+
+    ProductsHeaderComponent,
+     FiltersComponent,
+     ProductBoxComponent,
+     CartComponent,
+
+  ],
+    imports: [
+        BrowserModule,
+        AppRoutingModule,
+        BrowserAnimationsModule,
+        MatSidenavModule,
+        MatGridListModule,
+        MatMenuModule,
+        MatButtonModule,
+        MatCardModule,
+        MatIconModule,
+        MatExpansionModule,
+        MatGridListModule,
+        MatToolbarModule,
+        MatTableModule,
+        MatBadgeModule,
+        MatSnackBarModule,
+        MatListModule,
+        HttpClientModule,
+    ],
+  providers: [CartService , StoreService],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
